8007324a632ab122dad7049190d5ee5a84356c0e
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-09 19:37:44 -0500

    setup

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,30 @@
+
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const time = new Date().toLocaleString()
+  console.log(`[timestamp: ${time}] [method: ${req.method}] [url: ${req.url}]`)
+  next()
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUserId middleware')
+  next()
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUser middleware')
+  next()
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validatePost middleware')
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+logger,
+validateUserId,
+validateUser,
+validatePost
+}
